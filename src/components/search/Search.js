0568942ae64fd3922456cc590e03eb0c83d5e1ce
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-13 21:53:11 +0400

    new commit

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import { AsyncPaginate } from 'react-select-async-paginate'
+import React, { useState } from 'react';
+import { AsyncPaginate } from 'react-select-async-paginate';
 
-const search = () => {
+const Search = ({onSearchChange}) => {
+  const [search, setSearch] = useState(null);
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData)
+    onSearchChange(searchData)
+  }
+
   return (
-    <AsyncPaginate />
-  )
-}
+    <AsyncPaginate
+      placeholder='Search for city'
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+    />
+  );
+};
 
-export default search
+export default Search;

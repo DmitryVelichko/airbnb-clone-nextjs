4a2e2ac42fd959bdd52a426af06797b815a4a7eb
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-14 16:21:09 +0400

    new commit

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,13 @@ import Image from 'next/image'
 
 function Header() {
   return (
-    <header>
-        <h1>Header</h1>
-        <div>
+    <header className='sticky top-0 z-50'>
+        <div className='relative flex items-center h-10 cursor-pointer my-auto'>
             <Image 
             src='https://links.papareact.com/qd3'
             layout='fill'
+            objectFit='contain'
+            objectPosition='left'
             />
         </div>
         <div></div>

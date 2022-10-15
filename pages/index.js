fe02fc82d4a5f7ec64929d3657617dc416fad211
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-15 10:53:20 +0400

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import type { NextPage } from 'next'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className="">
       <Head>

8838e4701cc508b000a86b39259abe642bf6320b
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-15 21:31:57 +0400

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,58 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+import SmallCard from '../components/SmallCard';
 
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
-import Banner from '../components/Banner'
-import SmallCard from '../components/SmallCard'
-
-
-const Home = ({exploreData}) => {
+const Home = ({ exploreData }) => {
   return (
-    <div className="">
+    <div className=''>
       <Head>
         <title>Airbnb</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-     <Header />
-     <Banner />
+      <Header />
+      <Banner />
 
-     <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
 
-        {exploreData?.map(({img, distance, location}) => (
-          <SmallCard key={img} img={img} location={location} distance={distance}/>
-        ))}
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {exploreData?.map(({ img, distance, location }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                location={location}
+                distance={distance}
+              />
+            ))}
+          </div>
         </section>
-     </main>
 
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+        </section>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 export async function getStaticProps() {
-  const exploreData = await fetch('https://www.jsonkeeper.com/b/4G1G').then((res) => res.json())
+  const exploreData = await fetch('https://www.jsonkeeper.com/b/4G1G').then(
+    (res) => res.json()
+  );
+
+  const cardsData = await fetch('https://www.jsonkeeper.com/b/VHHT').then(
+    (res) => res.json()
+  );
+
   return {
     props: {
-      exploreData
-    }
-  }
+      exploreData,
+    },
+  };
 }

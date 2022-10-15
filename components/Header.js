710ed6c2c57e7a00f76d176c242f90141357ea88
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-15 10:48:41 +0400

    new commit

--- components/Header.js
+++ components/Header.js
@@ -21,10 +21,10 @@ function Header() {
 
         <div className='flex items-center space-x-4 justify-end text-gray-500'>
             <p>Become a host</p>
-            <GlobeAltIcon className='h-6'/> 
-            <div className='flex items-center space-x-2'>
-                <MenuIcon className='h-6' />
-                <UserCircleIcon className='h-6'/>
+            <GlobeAltIcon className='h-6 cursor-pointer'/> 
+            <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+                <MenuIcon className='h-6 cursor-pointer' />
+                <UserCircleIcon className='h-6 cursor-pointer'/>
             </div>
         </div>
     </header>

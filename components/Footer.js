5a4dcd7fc1059e8e3f7e28200215c48174512a85
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-16 23:44:17 +0400

    new commit

--- components/Footer.js
+++ components/Footer.js
@@ -11,7 +11,15 @@ function Footer() {
             <p>Airbnb Plus</p>
             <p>Airbnb Luxe</p>
         </div>
-        
+        <div className='space-y-4 text-xs text-gray-800'>
+            <h5 className='font-bold'>COMMUNITY</h5>
+            <p>Accessibility</p>
+            <p>Support</p>
+            <p>Safety</p>
+            <p>AirCover</p>
+            <p>Report a concern</p>
+        </div>
+      
     </div>
   )
 }

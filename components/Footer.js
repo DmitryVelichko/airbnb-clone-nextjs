a34df5bf38be80460a04dd321da66440a852d3f4
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-16 23:44:23 +0400

    new commit

--- components/Footer.js
+++ components/Footer.js
@@ -19,7 +19,15 @@ function Footer() {
             <p>AirCover</p>
             <p>Report a concern</p>
         </div>
-      
+        <div className='space-y-4 text-xs text-gray-800'>
+            <h5 className='font-bold'>HOST</h5>
+            <p>Try hosting</p>
+            <p>Visit our community forum</p>
+            <p>AirCover for Hosts</p>
+            <p>How to host responsibly</p>
+            <p>Explore hosting resources</p>
+        </div>
+       
     </div>
   )
 }

c3c27174aa59cf6823a68a8879f45c020f085252
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-20 22:05:19 +0400

    new commit

--- pages/search.js
+++ pages/search.js
@@ -6,6 +6,18 @@ function Search() {
   return (
     <div>
       <Header />
+      <main className='flex'>
+        <section>
+          <p className='text-xs'>300+ Stays for 5 number of guests</p>
+          <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
+          <div className=''>
+            <p className='button'>
+              Cancellation Flexibility
+            </p>
+            <p className='button'>Type of Place</p>
+          </div>
+        </section>
+      </main>
       <Footer />
     </div>
   );

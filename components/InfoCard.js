1669e7433f11995e6f83373ad0db8ba1148fcdc7
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-23 00:06:09 +0400

    new commit

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -14,7 +14,7 @@ const InfoCard = ({
   return (
     <div className='flex py-7 px-2 pr-4 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t'>
       <div className='relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0'>
-        <Image src={img} layout='fill' objectFit='cover' />
+        <Image src={img} layout='fill' objectFit='cover' className='rounded-2xl'/>
       </div>
 
       <div className='flex flex-col flex-grow pl-5'>

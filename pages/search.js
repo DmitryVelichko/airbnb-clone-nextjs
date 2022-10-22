28fca9b6f8b0ec5e6fff35e0b7bb9bebae65bbf4
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-22 16:46:12 +0400

    new commit

--- pages/search.js
+++ pages/search.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import {useRouter} from 'next/dist/client/router'
+import { useRouter } from 'next/dist/client/router';
+import { format } from 'date-fns';
+import InfoCard from '../components/InfoCard';
 
-function Search() {
+function Search({ searchResults }) {
+  const router = useRouter();
+  const { location, startDate, endDate, noOfGuests } = router.query;
 
-const router = useRouter()
-const {location, startDate, endDate, noOfGuests} = router.query;
+  const formattedStartDate = format(new Date(startDate), 'dd MMMM yyyy');
+  const formattedEndDate = format(new Date(endDate), 'dd MMMM yyyy');
+  const range = `${formattedStartDate} — ${formattedEndDate}`;
 
   return (
     <div>
-      <Header />
+      <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
+
       <main className='flex'>
         <section className='flex-grow pt-14 px-6'>
-          <p className='text-xs'>300+ Stays for 5 number of guests</p>
-          <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
+          <p className='text-xs'>
+            300+ Stays — {range} — for {noOfGuests} guests
+          </p>
+          <h1 className='text-3xl font-semibold mt-2 mb-6'>
+            Stays in {location}
+          </h1>
           <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
             <p className='button'>Cancellation Flexibility</p>
             <p className='button'>Type of Place</p>
@@ -22,6 +32,20 @@ const {location, startDate, endDate, noOfGuests} = router.query;
             <p className='button'>Rooms and Beds</p>
             <p className='button'>More Filters</p>
           </div>
+
+          {searchResults.map(
+            ({ img, location, title, description, star, price, total }) => (
+              <InfoCard
+                img={img}
+                location={location}
+                title={title}
+                description={description}
+                star={star}
+                price={price}
+                total={total}
+              />
+            )
+          )}
         </section>
       </main>
       <Footer />
@@ -30,3 +54,15 @@ const {location, startDate, endDate, noOfGuests} = router.query;
 }
 
 export default Search;
+
+export async function getSererSideProps() {
+  const searchResults = await fetch('https://www.jsonkeeper.com/b/5NPS').then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}

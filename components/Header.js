cd7de2465c93759ed0a0d8cd4551d54d9e902f94
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-24 14:52:47 +0400

    new commit

--- components/Header.js
+++ components/Header.js
@@ -14,12 +14,12 @@ import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
 import { useRouter } from 'next/dist/client/router';
 
-function Header({placeholder}) {
+function Header({ placeholder }) {
   const [searchInput, setSearchInput] = useState('');
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [noOfGuests, setNoOfGuests] = useState(1);
-  const router = useRouter()
+  const router = useRouter();
 
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate);
@@ -33,8 +33,8 @@ function Header({placeholder}) {
   };
 
   const resetInput = () => {
-    setSearchInput('')
-  }
+    setSearchInput('');
+  };
 
   const search = () => {
     router.push({
@@ -44,13 +44,30 @@ function Header({placeholder}) {
         startDate: startDate.toISOString(),
         endDate: endDate.toISOString(),
         noOfGuests,
-      }
-    })
-  }
+      },
+    });
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      router.push({
+        pathname: '/search',
+        query: {
+          location: searchInput,
+          startDate: startDate.toISOString(),
+          endDate: endDate.toISOString(),
+          noOfGuests,
+        },
+      });
+    }
+  };
 
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
-      <div onClick={() => router.push('/')} className='relative flex items-center h-10 cursor-pointer my-auto'>
+      <div
+        onClick={() => router.push('/')}
+        className='relative flex items-center h-10 cursor-pointer my-auto'
+      >
         <Image
           src='https://links.papareact.com/qd3'
           layout='fill'
@@ -59,19 +76,22 @@ function Header({placeholder}) {
         />
       </div>
 
-      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+      <div className='flex items-center min-[850px]:border-2 rounded-full py-2 min-[850px]:shadow-sm'>
         <input
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
+          onKeyDown={handleKeyDown}
           className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400'
           type='text'
-        placeholder={placeholder || 'Start your search'}
+          placeholder={placeholder || 'Start your search'}
         />
-        <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
+        <SearchIcon className='hidden min-[850px]:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
       </div>
 
       <div className='flex items-center space-x-4 justify-end text-gray-500'>
-        <p className='hidden md:inline cursor-pointer'>Become a host</p>
+        <p className='hidden min-[850px]:inline-flex cursor-pointer'>
+          Become a host
+        </p>
         <GlobeAltIcon className='h-6 cursor-pointer' />
         <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
           <MenuIcon className='h-6 cursor-pointer' />
@@ -101,8 +121,12 @@ function Header({placeholder}) {
           </div>
           <div>
             <div className='flex'>
-              <button onClick={resetInput} className='flex-grow text-gray-400'>Cancel</button>
-              <button onClick={search} className='flex-grow text-red-400'>Search</button>
+              <button onClick={resetInput} className='flex-grow text-gray-400'>
+                Cancel
+              </button>
+              <button onClick={search} className='flex-grow text-red-400'>
+                Search
+              </button>
             </div>
           </div>
         </div>
--- components/Map.js
+++ components/Map.js
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
-import ReactMapGL, {Marker, Popup} from 'react-map-gl';
-import  getCenter  from 'geolib/es/getCenter';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import getCenter from 'geolib/es/getCenter';
 
-function Map({searchResults}) {
+function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
 
   // Transform the search results object into the {latitude: 52.516272, longitude: 13.377722} object
-  const coordinates = searchResults.map(result => ({
+  const coordinates = searchResults.map((result) => ({
     longitude: result.long,
     latitude: result.lat,
-  }))
+  }));
 
   // The latitude and longitude of the center of locations coordinates
-  const center = getCenter(coordinates)
+  const center = getCenter(coordinates);
 
   const [viewport, setViewport] = useState({
     width: '100%',
     height: '100%',
     latitude: center.latitude,
     longitude: center.longitude,
-    zoom: 11
+    zoom: 11,
   });
 
   return (
@@ -26,16 +27,44 @@ function Map({searchResults}) {
       mapStyle='mapbox://styles/dmitry19892021/cl9l0667r000k14ntahufewbu'
       mapboxApiAccessToken={process.env.mapbox_key}
       {...viewport}
-      onViewportChange={(nextViewport) => {console.log(nextViewport);setViewport(nextViewport)}}
-      >
-        {searchResults.map(result => (
-            <div key={result.long}>
-              <Marker 
-              longitude={result.long}
+      onViewportChange={(nextViewport) => {
+        console.log(nextViewport);
+        setViewport(nextViewport);
+      }}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              onClick={() => setSelectedLocation(result)}
+              className='cursor-pointer text-2xl animate-bounce'
+              role='img'
+              aria-label='push-pin'
+            >
+              📌
+            </p>
+          </Marker>
+
+          {/*  Show popup if the marker is clicked on*/}
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
               latitude={result.lat}
-              ></Marker>
-            </div>
-        ))}
+              longitude={result.long}
+              closeOnClick={true}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
     </ReactMapGL>
   );
 }
